d17175e3000ab8a43ddfc7e8f6f6fe165ebd9c9b
bernard.cooley <bernard.cooley@global.com>
2024-08-07 10:36:17 +0100

    selector title

--- app/components/DeviceSelector.tsx
+++ app/components/DeviceSelector.tsx
@@ -8,32 +8,38 @@ interface Props {
 
 const DeviceSelector = ({ onSelect }: Props) => {
     return (
-        <Grid templateColumns={["repeat(2, 1fr)", "repeat(3, 1fr)"]} gap={8}>
-            {midiDevices
-                .filter((dev) => dev.deviceParamters.length > 0)
-                .map((device, index) => (
-                    <GridItem w="100%" key={device.name}>
-                        <Flex direction="column" alignItems="center">
-                            <Text>{device.name}</Text>
-                            <Button
-                                h="full"
-                                onClick={() => onSelect(index)}
-                                w="full"
-                                variant="unstyled"
-                                p={1}
-                                _hover={{
-                                    outline: "1px solid gray",
-                                    cursor: "pointer",
-                                    scale: 1.5,
-                                    shadow: "xl",
-                                }}
-                            >
-                                <Image src={device.imageSrc} />
-                            </Button>
-                        </Flex>
-                    </GridItem>
-                ))}
-        </Grid>
+        <Flex direction="column" alignItems="center" gap={2}>
+            <Text fontSize="xl">Choose a device</Text>
+            <Grid
+                templateColumns={["repeat(2, 1fr)", "repeat(3, 1fr)"]}
+                gap={8}
+            >
+                {midiDevices
+                    .filter((dev) => dev.deviceParamters.length > 0)
+                    .map((device, index) => (
+                        <GridItem w="100%" key={device.name}>
+                            <Flex direction="column" alignItems="center">
+                                <Text>{device.name}</Text>
+                                <Button
+                                    h="full"
+                                    onClick={() => onSelect(index)}
+                                    w="full"
+                                    variant="unstyled"
+                                    p={1}
+                                    _hover={{
+                                        outline: "1px solid gray",
+                                        cursor: "pointer",
+                                        scale: 1.5,
+                                        shadow: "xl",
+                                    }}
+                                >
+                                    <Image src={device.imageSrc} />
+                                </Button>
+                            </Flex>
+                        </GridItem>
+                    ))}
+            </Grid>
+        </Flex>
     );
 };
 
