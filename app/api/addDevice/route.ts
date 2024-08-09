207a0ac1a5dab774085d608c8c08b3d4738af7bb
bernard.cooley <bernard.cooley@global.com>
2024-08-09 14:19:06 +0100

    fixed manufacturer in schema

--- app/api/addDevice/route.ts
+++ app/api/addDevice/route.ts
@@ -1,20 +1,23 @@
+import { MidiDevice } from "@/app/types";
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
     const { data } = await req.json();
 
+    const d: MidiDevice[] = data;
+
     try {
-        for (const device of data) {
+        for (const device of d) {
             await prisma?.device.create({
                 data: {
                     name: device.name,
                     imageSrc: device.imageSrc,
                     deviceParamters: {
-                        create: device.deviceParamters.map((param: any) => ({
+                        create: device.deviceParamters.map((param) => ({
                             groupName: param.groupName,
                             ccs: {
-                                create: param.ccs.map((cc: any) => ({
+                                create: param.ccs.map((cc) => ({
                                     parameterName: cc.parameterName,
                                     number: cc.number,
                                 })),
